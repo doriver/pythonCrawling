594a6536ff4e822818cf11034ff54d3d1c57ba65
DESKTOP-RENTAL\user <tkdduq241@naver.com>
2024-12-23 12:28:10 +0900

    저게 selenium으로 js파고드는거 일듯

--- selenium/crawling/dhLottery/lotteryCrwl01.py
+++ selenium/crawling/dhLottery/lotteryCrwl01.py
@@ -12,3 +12,14 @@ bonus = num_comp[-1].text # 보너스 숫자도 빼먹으면 안됨
 
 print(numbers)
 print(bonus)
+
+
+
+#### num_view = driver.find_element(By.ID, 'numView') 결과물 
+# <selenium.webdriver.remote.webelement.WebElement 
+# (session="389b18b1dfbd10b3166b953a65076cd3"
+# , element="f.A42F4E2555CF2758C585963E6FC5B863.d.E695279D5F0206D6DB4050265252582B.e.120")>
+
+#### num_view.find_elements(By.XPATH, "*") 결과물
+# [ ] 안에 위와 똑같은 형태로 여러개 들어있음
+# <selenium.webdriver.remote.webelement.WebElement (session="389b18b1dfbd10b3166b953a65076cd3", element="f.A42F4E2555CF2758C585963E6FC5B863.d.E695279D5F0206D6DB4050265252582B.e.175")>]
