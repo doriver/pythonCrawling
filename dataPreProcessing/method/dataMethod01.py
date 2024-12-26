5343c98ff5d07f98def046263d74cec98841d9ca
DESKTOP-RENTAL\user <tkdduq241@naver.com>
2024-12-26 14:23:45 +0900

    파이썬이 D:\pythonCode01 경로를 모듈 탐색 경로에 포함하도록 설정 시스템 환경 변수에 PYTHONPATH 추가하는 방법도 있는데 , 코드에서 직접 모듈 경로를 추가했음

--- dataPreProcessing/method/dataMethod01.py
+++ dataPreProcessing/method/dataMethod01.py
@@ -3,7 +3,6 @@ def convert_k_to_numbers(num):
     if 'k' in num.lower(): # k있으면 조작해서 반환
         number = float(num.lower().replace('k', ''))
         return int(number * 1000)
-    return int(num) # k없으면 그냥 반환
+    else:
+        return int(num) # k없으면 그냥 반환
 
-ex = "1.7k"
-print(convert_k_to_numbers(ex)) # 1700
--- dataPreProcessing/test/methodTest01.py
+++ dataPreProcessing/test/methodTest01.py
@@ -1,9 +1,10 @@
+import sys
+import os
+# 프로젝트 루트 경로를 sys.path에 추가
+sys.path.append(os.path.abspath("D:/pythonCode01"))
+
 # 파이썬에서 다른 .py 파일에 정의된 메서드나 사용하려면 import를 활용
 import dataPreProcessing.method.dataMethod01 as dm
 
-# 상대 경로 import
-# from ..method.dataMethod01 import 함수이름
-
-
 ex = "1.7k"
-print(dm.convert_k_to_numbers(ex)) # 
+print(dm.convert_k_to_numbers(ex)) # 1700
